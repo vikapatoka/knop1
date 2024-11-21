2c1e323ffca693b0163c114a9a6575290f47e166
vikapatoka <vikapatoka2006@yandex.ru>
2024-11-21 13:51:35 +0300

    Переход на другую страницу

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView helloText;
+    private ImageButton startButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+            helloText = (TextView) findViewById(R.id.helloText);
+            startButton = (ImageButton) findViewById(R.id.startButton);
+    }
+
+    public void onStartButtonClick(View view) {
+        startButton.setImageResource(R.drawable.robot);
+        helloText.setVisibility(View.VISIBLE);
+        startButton.setClickable(false);
+    }
+    public void Click(View view) {
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+        finish();
+    }
+}
